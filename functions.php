57e6b69939530b7dbad5a10c7e5fa2c8c302fd30
coraline_perrot_lp <coraline.perrot35@gmail.com>
2018-03-26 16:34:00 +0200

    test custom

--- functions.php
+++ functions.php
@@ -14,23 +14,39 @@ function register_my_menu() {
 
 /* Custom Post Type */
 
-add_action('init', 'cpt');
-function cpt() {
-    register_post_type('recette', [
-        'labels' => [
-            'name' => 'Nos Recette',
-            'singular_name' => 'Recette'
-        ],
-        'public' => true,
-        'has_archive' => true,
-        'supports' => ['title', 'editor', 'excerpt', 'thumbnail']
-    ]);
+/*
+* On utilise une fonction pour créer notre custom post type 'Séries TV'
+*/
 
-    register_taxonomy('categorie', ['dessert'], [
-        'labels' => [
-            'name' => 'Desserts',
-            'singular_name' => 'Dessert'
-        ],
-        'hierarchical' => true
-    ]);
+function wpm_custom_post_type() {
+
+	// On rentre les différentes dénominations de notre custom post type qui seront affichées dans l'administration
+	$labels = array(
+		// Le nom au pluriel
+		'name'                => _x( 'Nos Recettes', 'Post Type General Name'),
+		// Le nom au singulier
+		'singular_name'       => _x( 'Recette', 'Post Type Singular Name'),
+	);
+	
+	// On peut définir ici d'autres options pour notre custom post type
+	
+	$args = array(
+		'label'               => __( 'Nos Recettes'),
+		// On définit les options disponibles dans l'éditeur de notre custom post type ( un titre, un auteur...)
+		'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', ),
+		/* 
+		* Différentes options supplémentaires
+		*/	
+		'hierarchical'        => false,
+		'public'              => true,
+		'has_archive'         => true,
+		'rewrite'			  => array( 'slug' => 'recette'),
+
+	);
+	
+	// On enregistre notre custom post type qu'on nomme ici "serietv" et ses arguments
+	register_post_type( 'recette', $args );
+
 }
+
+add_action( 'init', 'wpm_custom_post_type', 0 );
