d877c10da8b7b283924798941ed9d6d782a6f503
coraline_perrot_lp <coraline.perrot35@gmail.com>
2018-05-14 14:57:28 +0200

    modification css + add header

--- header.php
+++ header.php
@@ -8,8 +8,14 @@
 
 <body <?php body_class() ?>>
     
+    <div class="background-header">
+        <div class="overlay">
+        <img src="<?php echo( get_header_image() ); ?>" alt="<?php echo( get_bloginfo( 'title' ) ); ?>" />
+        
             <?php wp_nav_menu( array( 'blogcuisine' => 'menu-header' ) ); ?>
-
+        </div>
+        
+    </div>
     
- 
+     
   
